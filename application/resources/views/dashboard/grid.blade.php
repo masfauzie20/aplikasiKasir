3a0872d6fc0a7c87c98a2bf80e709588010618d9
inibukanfauzie <achmadfauzie20@gmail.com>
2019-10-23 08:23:38 +0700

    edit repo

--- application/resources/views/dashboard/grid.blade.php
+++ application/resources/views/dashboard/grid.blade.php
@@ -1,46 +1,95 @@
-@extends('layouts.app') @section('content')
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<!-- MAIN CONTENT -->
-<div class="main-content">
-    <div class="container-fluid">
-        <center>
-            <h3 class="page-title" style="margin-top: 180px;">Selamat Datang!</h3>
-        </center>
-        {{-- <div class="row">
-            <div class="col-md-12">
-                <div class="panel">
-                    <div class="panel-heading">
-                        <select id="year" name="year" class="select form-control">
-                            <option value="" hidden="">Pilih Tahun</option>
-                            {{-- @foreach($tahun as $datas) 
-                            <option id="chooseYear" value="{{$datas->tahun}}">{{$datas->tahun}}</option>
-                            @endforeach
-                        </select>
-                    </div>
-                     @if($terlambat->count())
-                    {!! $chart->html() !!}
-                </div>
-                <hr>
-                 @else
-                        &nbsp&nbsp&nbsp<i class="fa fa-exclamation-triangle"></i> Data Tidak Tersedia
-                 @endif
-            </div> --}}
-            <!-- END MAIN CONTENT -->
-            {{-- {!! Charts::scripts() !!} {!! $chart->script() !!} --}}
-<script>
-$(document).ready(function () {
-    $('select').find('option').click(function (e) {
-        e.preventDefault();
-        year = e.currentTarget.getAttribute('value');
-        
-        window.location.href = "{{ route('dashboard') }}?year=" + year
-    })
-})
+    <title>Laravel</title>
 
-$(".select option").val(function(idx, val) {
-    $(this).siblings('[value="' + val + '"]').remove();
-    return val;
-});
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
-</script>
-@endsection
+    <!-- Styles -->
+    <style>
+        html, body {
+            background-color: #fff;
+            color: #636b6f;
+            font-family: 'Raleway', sans-serif;
+            font-weight: 100;
+            height: 100vh;
+            margin: 0;
+        }
+
+        .full-height {
+            height: 100vh;
+        }
+
+        .flex-center {
+            align-items: center;
+            display: flex;
+            justify-content: center;
+        }
+
+        .position-ref {
+            position: relative;
+        }
+
+        .top-right {
+            position: absolute;
+            right: 10px;
+            top: 18px;
+        }
+
+        .content {
+            text-align: center;
+        }
+
+        .title {
+            font-size: 84px;
+        }
+
+        .links > a {
+            color: #636b6f;
+            padding: 0 25px;
+            font-size: 12px;
+            font-weight: 600;
+            letter-spacing: .1rem;
+            text-decoration: none;
+            text-transform: uppercase;
+        }
+
+        .m-b-md {
+            margin-bottom: 30px;
+        }
+    </style>
+</head>
+<body>
+<div class="flex-center position-ref full-height">
+    @if (Route::has('login'))
+        <div class="top-right links">
+            @auth
+                <a href="{{ url('/home') }}">Home</a>
+            @else
+                <a href="{{ route('login') }}">Login</a>
+                <a href="{{ route('register') }}">Register</a>
+            @endauth
+        </div>
+    @endif
+
+    <div class="content">
+        <div class="title m-b-md">
+            Laravel
+        </div>
+
+        <div class="links">
+            <a href="https://laravel.com/docs">Documentation</a>
+            <a href="https://laracasts.com">Laracasts</a>
+            <a href="https://laravel-news.com">News</a>
+            <a href="https://forge.laravel.com">Forge</a>
+            <a href="https://github.com/laravel/laravel">GitHub</a>
+        </div>
+    </div>
+</div>
+</body>
+</html>
